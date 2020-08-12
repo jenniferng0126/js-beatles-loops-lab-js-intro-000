6188361fef35c7e997fa4b15d0a4c53dfa183819
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 14:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musiciansArr, instrumentsArr) {
+  var out = [];
+  for (let i=0; i < musiciansArr.length; i++) {
+    out.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`);
+  }
+  return out;
+}
